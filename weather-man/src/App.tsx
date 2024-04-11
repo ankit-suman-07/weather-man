c6152e66917302ef5261957cd9590f3ce45a7aaf
ankit-suman-07 <ankitsuman07@gmail.com>
2024-04-11 20:38:22 +0530

    first commit

---
+++ weather-man/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        The Weather Man
+      </header>
+    </div>
+  );
+}
+
+export default App;
